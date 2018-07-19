366d2fdd5be397d027a21ffbdc3bb5bc58e734c1
An Huynh <huynha1@wit.edu>
2018-07-19 18:36:44 -0400

    Testing out changes to html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+// Base modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+// Angular Material
+import { AppMaterialModules } from './material.module';
+
+// ngx-bootstrap
+import { AccordionModule, AlertModule, ButtonsModule, CarouselModule, CollapseModule, BsDatepickerModule,
+  BsDropdownModule, ModalModule, PaginationModule, PopoverModule, ProgressbarModule, RatingModule,
+  SortableModule, TabsModule, TimepickerModule, TooltipModule, TypeaheadModule } from 'ngx-bootstrap';
+
+// Pages for website
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppMaterialModules,
+    // ngx-bootstrap modules
+    AccordionModule.forRoot(),
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    PopoverModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    RatingModule.forRoot(),
+    SortableModule.forRoot(),
+    TabsModule.forRoot(),
+    TimepickerModule.forRoot(),
+    TooltipModule.forRoot(),
+    TypeaheadModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
