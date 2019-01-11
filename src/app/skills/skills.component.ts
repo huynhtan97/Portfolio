792e7cdf45d148e421a00ca83685b49e6d20afe8
An Huynh <huynha1@wit.edu>
2019-01-10 21:11:10 -0500

    Created basic template for skills page and planning on adding hobbies component soon

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -21,29 +21,44 @@ export interface SoftSkills {
 export class SkillsComponent {
 
   constructor() { }
-  // To populate mat-chips
+  /**
+   * @type {TechSkills[]}
+   * @memberof SkillsComponent
+   * if years >= 3, then color = warn
+   * if years == 2, then color = accent
+   * if years < 2, then color = primary
+   */
   languagesAndColors: TechSkills[] = [
-    {skill: 'Java', years: '4+ years', color: 'accent'},
-    {skill: 'Javascript', years: '2+ years', color: 'primary'},
-    {skill: 'jQuery', years: '2+ years', color: 'accent'},
-    {skill: 'Python', years: '2+ years', color: 'warn'},
-    {skill: 'PHP', years: '1+ years', color: 'accent'},
-    {skill: 'HTML', years: '2+ years', color: 'primary'},
-    {skill: 'CSS/Sass/Bootstrap', years: '2+ years', color: 'accent'},
-    {skill: 'C', years: '1+ years', color: 'warn'},
-    {skill: 'C++', years: '3+ years', color: 'accent'},
+    {skill: 'Java', years: '4+ years', color: 'warn'},
+    {skill: 'Javascript', years: '3+ years', color: 'warn'},
+    {skill: 'jQuery', years: '1+ years', color: 'primary'},
+    {skill: 'Python', years: '2+ years', color: 'accent'},
+    {skill: 'PHP', years: '1+ years', color: 'primary'},
+    {skill: 'HTML', years: '3+ years', color: 'warn'},
+    {skill: 'CSS/Sass/Bootstrap', years: '3+ years', color: 'warn'},
+    {skill: 'C', years: '1+ years', color: 'primary'},
+    {skill: 'C++', years: '1+ years', color: 'primary'},
     {skill: 'Shell scripting', years: '1+ years', color: 'primary'},
-    {skill: 'Scala', years: '1+ years', color: 'accent'},
-    {skill: 'Ruby', years: '2+ years', color: 'warn'}
+    {skill: 'Scala', years: '1+ years', color: 'primary'},
+    {skill: 'Ruby', years: '2+ years', color: 'accent'},
+    {skill: 'Matlab', years: '2+ years', color: 'accent'},
+    {skill: 'Markdown', years: '3+ years', color: 'warn'},
+    {skill: 'LaTeX', years: '1+ years', color: 'primary'},
+  ];
+
+  toolsAndColors: TechSkills[] = [
+    {skill: 'AWS', years: '2+ years', color: 'accent'},
+    {skill: 'Google Cloud', years: '1+ years', color: 'primary'},
+    {skill: 'IBM Cloud', years: '1+ years', color: 'primary'}
   ];
 
   frameworksAndColors: TechSkills[] = [
     {skill: 'AngularJS', years: '2+ years', color: 'accent'},
-    {skill: 'Rails', years: '2+ years', color: 'primary'}
+    {skill: 'Rails', years: '2+ years', color: 'accent'}
   ];
 
   softwareAndColors: TechSkills[] = [
-    {skill: 'AngularJS', years: '2+ years', color: 'accent'},
+    {skill: 'Microsoft Office', years: '8+ years', color: 'warn'},
     {skill: 'Rails', years: '2+ years', color: 'primary'},
     {skill: 'AngularJS', years: '2+ years', color: 'accent'},
     {skill: 'Rails', years: '2+ years', color: 'primary'},
@@ -51,19 +66,26 @@ export class SkillsComponent {
     {skill: 'Rails', years: '2+ years', color: 'primary'}
   ];
 
+  /**
+   * @type {SoftSkills[]}
+   * @memberof SkillsComponent
+   * if from == Internships, then color = warn
+   * if from == School Clubs, then color = accent
+   * if from == (Anything else), then color = primary
+   */
   softskillsAndColor: SoftSkills[] = [
-    {skill: 'Organization', from: '4+ years', color: 'accent'},
-    {skill: 'Teamwork', from: '4+ years', color: 'primary'},
-    {skill: 'Team leadership', from: '4+ years', color: 'accent'},
-    {skill: 'Warn', from: '4+ years', color: 'warn'},
-    {skill: 'none', from: '4+ years', color: 'accent'},
-    {skill: 'Primary', from: '4+ years', color: 'primary'},
-    {skill: 'Accent', from: '4+ years', color: 'accent'},
-    {skill: 'Warn', from: '4+ years', color: 'warn'},
-    {skill: 'none', from: '4+ years', color: 'accent'},
-    {skill: 'Primary', from: '4+ years', color: 'primary'},
-    {skill: 'Accent', from: '4+ years', color: 'accent'},
-    {skill: 'Warn', from: '4+ years', color: 'warn'}
+    {skill: 'Organization', from: 'School', color: 'primary'},
+    {skill: 'Teamwork', from: 'School Clubs', color: 'accent'},
+    {skill: 'Team Leadership', from: 'School Clubs', color: 'accent'},
+    {skill: 'Public Speaking', from: 'Self-taught', color: 'primary'},
+    {skill: 'Communication', from: 'School+Internships', color: 'warn'},
+    {skill: 'Accountability', from: 'School Clubs+Internships', color: 'warn'},
+    {skill: 'Time Management', from: 'School Clubs', color: 'accent'},
+    {skill: 'Listening', from: 'School', color: 'primary'},
+    {skill: 'Adaptability', from: 'School Clubs+Internships', color: 'warn'},
+    {skill: 'Quick Learner', from: 'Internships', color: 'primary'},
+    {skill: 'Problem-Solving', from: 'School Clubs+Internships', color: 'warn'},
+    {skill: 'Self-Motivation', from: 'School', color: 'primary'}
   ];
 
 }
