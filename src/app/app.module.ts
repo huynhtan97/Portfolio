99e8a0cd3250bd61a5a5bf7a952734f98853b660
An Huynh <huynha1@wit.edu>
2019-01-10 19:50:49 -0500

    Added Medium link and directed viewers to menu button in homepage description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 // Angular Material
+import 'hammerjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModules } from './material.module';
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+
+export interface TechSkills {
+  skill: String;
+  years: String;
+  color: ThemePalette;
+}
+
+export interface SoftSkills {
+  skill: String;
+  from: String;
+  color: ThemePalette;
+}
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent {
 
   constructor() { }
+  // To populate mat-chips
+  languagesAndColors: TechSkills[] = [
+    {skill: 'Java', years: '4+ years', color: 'accent'},
+    {skill: 'Javascript', years: '2+ years', color: 'primary'},
+    {skill: 'jQuery', years: '2+ years', color: 'accent'},
+    {skill: 'Python', years: '2+ years', color: 'warn'},
+    {skill: 'PHP', years: '1+ years', color: 'accent'},
+    {skill: 'HTML', years: '2+ years', color: 'primary'},
+    {skill: 'CSS/Sass/Bootstrap', years: '2+ years', color: 'accent'},
+    {skill: 'C', years: '1+ years', color: 'warn'},
+    {skill: 'C++', years: '3+ years', color: 'accent'},
+    {skill: 'Shell scripting', years: '1+ years', color: 'primary'},
+    {skill: 'Scala', years: '1+ years', color: 'accent'},
+    {skill: 'Ruby', years: '2+ years', color: 'warn'}
+  ];
 
-  ngOnInit() {
-  }
+  frameworksAndColors: TechSkills[] = [
+    {skill: 'AngularJS', years: '2+ years', color: 'accent'},
+    {skill: 'Rails', years: '2+ years', color: 'primary'}
+  ];
+
+  softwareAndColors: TechSkills[] = [
+    {skill: 'AngularJS', years: '2+ years', color: 'accent'},
+    {skill: 'Rails', years: '2+ years', color: 'primary'},
+    {skill: 'AngularJS', years: '2+ years', color: 'accent'},
+    {skill: 'Rails', years: '2+ years', color: 'primary'},
+    {skill: 'AngularJS', years: '2+ years', color: 'accent'},
+    {skill: 'Rails', years: '2+ years', color: 'primary'}
+  ];
+
+  softskillsAndColor: SoftSkills[] = [
+    {skill: 'Organization', from: '4+ years', color: 'accent'},
+    {skill: 'Teamwork', from: '4+ years', color: 'primary'},
+    {skill: 'Team leadership', from: '4+ years', color: 'accent'},
+    {skill: 'Warn', from: '4+ years', color: 'warn'},
+    {skill: 'none', from: '4+ years', color: 'accent'},
+    {skill: 'Primary', from: '4+ years', color: 'primary'},
+    {skill: 'Accent', from: '4+ years', color: 'accent'},
+    {skill: 'Warn', from: '4+ years', color: 'warn'},
+    {skill: 'none', from: '4+ years', color: 'accent'},
+    {skill: 'Primary', from: '4+ years', color: 'primary'},
+    {skill: 'Accent', from: '4+ years', color: 'accent'},
+    {skill: 'Warn', from: '4+ years', color: 'warn'}
+  ];
 
 }
